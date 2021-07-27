057e26a32db3cf08c312f8a3e59c874d514c5d88
Megan Thong <meganthong@outlook.com>
2021-07-27 17:16:06 +0800

    simple colour picker

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,15 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colours = ["green", "red", "rgba(133,122,200)", "#f15025"]
+const btn = document.getElementById("btn")
+const colour = document.querySelector(".colour")
+
+btn.addEventListener("click", function() {
+    const randomNumber = getRandomNumber()
+    console.log(randomNumber)
+    document.body.style.backgroundColor = colours[randomNumber]
+    colour.textContent = colours[randomNumber]
+})
+
+// get random number between 0 and 3
+function getRandomNumber() {
+    return Math.floor(Math.random() * colours.length) 
+}
